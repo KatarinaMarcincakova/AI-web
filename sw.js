2128202110db8e40ca54703f0f3b3dc71980921d
StanislavJochman <s.jochman@gmail.com>
2020-03-09 21:18:41 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,56 @@
+var cacheVersion = 1;
+var currentCache = {
+  offline: 'offline-cache' + cacheVersion
+};
+const offlineUrl = 'index.html';
+
+this.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(currentCache.offline).then(function(cache) {
+      return cache.addAll([
+          offlineUrl,
+          //obrazky
+          'img/aakoamen.jpg',
+          'img/abeceda.jpg',
+          'img/nocnyrybispev.png',
+          'img/slovnazasoba.png',
+          'img/komunikacny_retazec.jpg',
+          //scss
+          'scss/resume.scss',
+          //js
+          'js/script.js',
+          //css
+          'css/style.css'
+      ]);
+    })
+  );
+});
+this.addEventListener('fetch', event => {
+  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
+        event.respondWith(
+          fetch(event.request.url).catch(error => {
+              return caches.match(offlineUrl);
+          })
+    );
+  }
+  else{
+        event.respondWith(caches.match(event.request)
+                        .then(function (response) {
+                        console.log("navigate not supported");
+                        return response || fetch(event.request);
+                    })
+            );
+      }
+});
+
+self.addEventListener('push', function(event) {
+
+  const title = 'Stránka zmenená';
+  const options = {
+    body: `${event.data.text()}`,
+    icon: 'icon512x512.png',
+    badge: 'icon512x512.png'
+  };
+
+  event.waitUntil(self.registration.showNotification(title, options));
+});
