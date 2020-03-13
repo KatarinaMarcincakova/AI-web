576fab8f65f3d0a2f068a7c6120fb2889be6683d
KatarinaMarcincakova <katulik137@gmail.com>
2020-03-13 17:23:33 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,10 +11,44 @@ this.addEventListener('install', event => {
           offlineUrl,
           //obrazky
           'images/IPDH.gif',
+          'images/cyklus.gif',
+          'images/OPDH.gif',
+          'images/OV.gif',
+          'images/PDH.gif',
+          'images/PDS.gif',
+          'images/PPC.gif',
+          'images/katka.jpg',
+          'images/lenka.jpg',
+          'images/ai-bg.jpg',
+          'images/er.png',
+          'images/klasif.png',
+          'images/klasifln.png',
+          'images/klasiflo.png',
+          'images/lr.png',
+          'images/mdtab.png',
+          'images/nltablm.png',
+          'images/nltabm.png',
+          'images/pr.png',
+          'images/pr3.png',
+          'images/pr4.png',
+          'images/quina.png',
+          'images/strom.png',
+          'images/table1.png',
+          'images/table2.png',
+          'images/tablova.png',
           //js
           'script.js',
           //css
-          'style.css'
+          'style.css',
+          //html
+          in.html,
+          index.html,
+          nl.html,
+          nv.html,
+          ns.html,
+          sh.html,
+          uav.html,
+          uvod.html        
       ]);
     })
   );
