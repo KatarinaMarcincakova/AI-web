b4e05d31f3dd43adbfd6d2d63d84ba8874517aa8
StanislavJochman <s.jochman@gmail.com>
2020-03-13 21:30:30 +0100

    commit

--- sw.js
+++ sw.js
@@ -53,32 +53,3 @@ this.addEventListener('install', event => {
     })
   );
 });
-this.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate' || (event.request.method === 'GET' && event.request.headers.get('accept').includes('text/html'))) {
-        event.respondWith(
-          fetch(event.request.url).catch(error => {
-              return caches.match(offlineUrl);
-          })
-    );
-  }
-  else{
-        event.respondWith(caches.match(event.request)
-                        .then(function (response) {
-                        console.log("navigate not supported");
-                        return response || fetch(event.request);
-                    })
-            );
-      }
-});
-
-self.addEventListener('push', function(event) {
-
-  const title = 'Stránka zmenená';
-  const options = {
-    body: `${event.data.text()}`,
-    icon: 'icon512x512.png',
-    badge: 'icon512x512.png'
-  };
-
-  event.waitUntil(self.registration.showNotification(title, options));
-});
