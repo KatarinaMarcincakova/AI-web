4ce13aede0916adab1a7bd972886919ef5debc21
KatarinaMarcincakova <katulik137@gmail.com>
2020-04-18 00:52:15 +0200

    Add files via upload

--- presentation/script.js
+++ presentation/script.js
@@ -2,7 +2,7 @@ slideN = 0
 
 
 for (i = 1; i < 6; i++) {
-	$('content').eq(i).addClass('hidden');
+	$('..content').eq(i).addClass('hidden');
   }
 function myFunction(event) {
   var x = event.which;
@@ -10,21 +10,21 @@ function myFunction(event) {
   
   if (x == 39){
 	for (i = 0; i < 6; i++) {
-		$('content').eq(i).removeClass('shown');
-		$('content').eq(i).addClass('hidden');
+		$('.content').eq(i).removeClass('shown');
+		$('.content').eq(i).addClass('hidden');
 		console.log("for");
 	}
 	slideN = slideN + 1;
-    $('content').eq(slideN).addClass('shown');
+    $('.content').eq(slideN).addClass('shown');
   }
   if (x == 37){
 	for (i = 0; i < 6; i++) {
-		$('content').eq(i).removeClass('shown');
-		$('content').eq(i).addClass('hidden');
+		$('.content').eq(i).removeClass('shown');
+		$('.content').eq(i).addClass('hidden');
 		console.log("for");
 	}
 	slideN = slideN - 1;
-    $('content').eq(slideN).addClass('shown');
+    $('.content').eq(slideN).addClass('shown');
   }
   console.log(slideN);
 }
