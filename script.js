63da8576be998dc6fe6b0ac16d8b5263ce39fb8d
KatarinaMarcincakova <katulik137@gmail.com>
2020-05-15 00:27:56 +0200

    prihlasenie / registracia modal + copy dole

--- script.js
+++ script.js
@@ -31,4 +31,17 @@ function s_close() {
   sidebar.style.display = "none";
 }
 
+var m = document.getElementsByClassName("modal");
+//var span = document.getElementsByClassName("close")[0];
+
+function close_modal(numM) {
+  m[numM].style.display = "none";
+  //console.log("close")
+}
+
+function open_modal(numM) {
+  m[numM].style.display = "block";
+  //console.log("open")
+}
+
         
